f1e1eb4468252e1b93304036060547a9b777cdc3
Rivaldo-Nofrizal <rivaldonovrizal2019@gmail.com>
2025-03-19 06:06:32 +0700

    Add files via upload

---
+++ Tugas_Modul3.py
@@ -0,0 +1,10 @@
+M = int(input("Masukkan Modal Awal: "))
+r = float(input("Masukkan Suku Bungan Tahunan (%): "))
+T = float(input("Masukkan Target Investasi: "))
+tahun = 0
+for tahun in range (1,M) :
+    M += M*(r/100)
+    print  (f"Tahun ke-{tahun} : Rp{M:}")
+    if M>=T:
+        break
+print(f"Target tercapai dalam {tahun} tahun!")
